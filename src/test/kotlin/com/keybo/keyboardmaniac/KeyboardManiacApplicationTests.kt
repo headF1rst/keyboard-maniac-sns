0eaac2ffd0c8bd9d4470d2db8dbc3bcd2766002d
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-05-25 02:25:21 +0900

    init

---
+++ src/test/kotlin/com/keybo/keyboardmaniac/KeyboardManiacApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keybo.keyboardmaniac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeyboardManiacApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
